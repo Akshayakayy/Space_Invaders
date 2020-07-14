fc6168c1bc142f5538e3f6701c0ee6b7d55b822d
avani17101 <staravani1999@gmail.com>
2020-07-14 13:54:52 +0530

    add timer img

--- visual/js/view.js
+++ visual/js/view.js
@@ -418,6 +418,7 @@ var View = {
     },
     showStats: function(opts) {
         var texts = [
+
             'length: ' + Math.round(opts.pathLength * 100) / 100,
             'time: ' + opts.timeSpent + 'ms',
             'operations: ' + opts.operationCount
